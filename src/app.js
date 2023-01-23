78d2e6b8a911d56e9477d856041e74b80631ac57
Fernando <fh1273509@gmail.com>
2023-01-22 22:27:26 -0300

    terminamos el server basico

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const config = require('./config');
+const clientes = require('./modulos/clientes/rutas.js');
 
 const app = express();
 
+//configuracion------------------------------------------------
 app.set('port', config.app.port);
+
+//rutas--------------------------------------------------------
+app.use('/api/clientes', clientes);
+
+
 
 module.exports = app;
