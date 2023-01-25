cc55db3d0f210dff4fd8538e6c4748f99edefabb
Fernando <fh1273509@gmail.com>
2023-01-24 21:41:22 -0300

    respuestas de mensaje ok y error

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const respuestas = require('../../red/respuestas');
 
 
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.send('Clientes ok');
+    respuestas.success(req, res, 'Todo Ok desde clientes', 200);
 });
 
 module.exports = router;
