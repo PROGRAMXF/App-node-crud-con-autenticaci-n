231cba284cdb8bf7f35a4ddb21eb6089ae6a291d
Fernando <fh1273509@gmail.com>
2023-02-01 20:26:46 -0300

    continuamos modificando las rutas antes de trabajar con los webtokens

--- src/modulos/clientes/controlador.js
+++ src/modulos/clientes/controlador.js
@@ -1,25 +1,36 @@
 //desde aqui vamos a hacer las consultas a la bd
-const db = require('../../DB/mysql');
+
 
 const TABLA = 'clientes';
 
-function todos(){
-    return db.todos(TABLA);
-}
-function uno(id){
-    return db.uno(TABLA, id);
-}
-function agregar(body){
-    return db.agregar(TABLA, body);
-}
-function eliminar(body){
-    return db.eliminar(TABLA, body);
-}
+module.exports = function(dbInyectada) {
 
+    let db = dbInyectada;
 
-module.exports = {
-    todos,
-    uno,
-    agregar,
-    eliminar
+    if(!db) {
+        db = require('../../DB/mysql');
+
+    }
+
+    function todos(){
+        return db.todos(TABLA);
+    }
+    function uno(id){
+        return db.uno(TABLA, id);
+    }
+    function agregar(body){
+        return db.agregar(TABLA, body);
+    }
+    function eliminar(body){
+        return db.eliminar(TABLA, body);
+    }
+
+    return {
+        todos,
+        uno,
+        agregar,
+        eliminar
+    }
+
+    
 }
---
+++ src/modulos/index.js
@@ -0,0 +1,8 @@
+//constructor al quwe le pasamos nuestra bd
+
+const db = require('../../src/DB/mysql');
+const ctrl = require('./clientes/controlador');
+
+
+//exportamos el modulo
+module.exports = ctrl(db);
